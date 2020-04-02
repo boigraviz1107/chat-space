25867231fe5427be6f0424b21062171be87ec6f5
boigraviz1107 <soccer.117@icloud.com>
2020-04-02 15:22:59 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(function() {
   function buildHTML(message)   {
+   
 
   var html_user = `<div class="chatmain-messeage_list_box_data">
                      <div class="chatmain-messeage_list_box_data_name"> 
@@ -8,14 +9,14 @@ $(function() {
                      <div class="chatmain-messeage_list_box_data_day">
                       ${message.created_at} 
                      </div>
-                   </div>` 
+                    </div>`
   var html_content = `<p class="chatmain-messeage_list_box_masseage__content">
                       ${message.content} 
                       </p>` 
   var html_image = `<img src="` + message.image + `" class="chatmain-messeage_list_box_masseage__image" >`
  
     if (message.content && message.image) {
-      var html = `<div class="chatmain-messeage_list_box">
+      var html = `<div class="chatmain-messeage_list_box" data-id=${message.id}>
                    ${html_user} 
                    <div class="chatmain-messeage_list_box_masseage">
                     ${html_content}
@@ -24,7 +25,7 @@ $(function() {
                  </div>`
                  return html;
     } else if (message.content) {
-      var html = `<div class="chatmain-messeage_list_box"> 
+      var html = `<div class="chatmain-messeage_list_box" data-id=${message.id}> 
                   ${html_user} 
                    <div class="chatmain-messeage_list_box_masseage"> 
                   ${html_content}
@@ -32,7 +33,7 @@ $(function() {
                   </div>`
         return html;
     }else if (message.image) {
-      var html = `<div class="chatmain-messeage_list_box"> 
+      var html = `<div class="chatmain-messeage_list_box" data-id=${message.id}> 
                   ${html_user} 
                    <div class="chatmain-messeage_list_box_masseage">
                   ${html_image}
@@ -41,7 +42,30 @@ $(function() {
         return html;
     };
   }
-
+  
+  var reloadMessages = function() {
+    var last_message_id = $('.chatmain-messeage_list_box:last').data("id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.chatmain-messeage_list').append(insertHTML);
+        $('.chatmain-messeage_list').animate({ scrollTop: $('.chatmain-messeage_list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
     
   
   $('#new_message').on('submit', function(e){
@@ -68,4 +92,7 @@ $(function() {
       alert("メッセージ送信に失敗しました");
   });
   })
+   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+     setInterval(reloadMessages, 7000);
+   }
   });
